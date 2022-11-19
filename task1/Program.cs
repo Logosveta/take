34f2074a99213e490041282144a53f19830f066a
Svetlana <logosvet1@mail.ru>
2022-11-19 20:56:12 +0300

    1 задача

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int Number1 = new int();
+Console.WriteLine("Введите первое число:");
+Number1 = Convert.ToInt32(Console.ReadLine());
+int Number2 = new int();
+Console.WriteLine("Введите второе число:");
+Number2 = Convert.ToInt32(Console.ReadLine());
+if (Number1 < Number2)
+{
+    Console.Write("Большее число:");
+    Console.WriteLine(Number2);
+}
+else
+{
+    Console.Write("Большее число:");
+    Console.WriteLine(Number1);
+}
